f20ba377cab75818ac1b893eccf3c9b3e85db407
ajaysth <shrestha11ajay@gmail.com>
2026-04-21 11:41:57 +0545

    server created along with dependencies

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get("/",(req,res)=>{
     res.json({message:"Hello World"});
