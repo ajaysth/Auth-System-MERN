a44ffc02d063557d66aee6ad2a4c55d48bf8f5ee
ajaysth <shrestha11ajay@gmail.com>
2026-04-21 11:36:11 +0545

    server created along with dependencies

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config" 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.json({message:"Hello World"});
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+})
