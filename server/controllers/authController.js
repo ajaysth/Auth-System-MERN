f5fdf76e68c261bddc1a1fccd56a158285ef8396
ajaysth <shrestha11ajay@gmail.com>
2026-04-22 17:11:56 +0545

    email functionality implemented using nodemailer

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import transporter from "../config/nodemailer.js";
 
 const register = async (req,res)=>{
     const {name,email,password} = req.body;
@@ -37,6 +38,15 @@ const register = async (req,res)=>{
             sameSite:process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge:7*24*60*60*1000
         })
+
+        const mailOptions = {
+            from : process.env.SENDER_EMAIL,
+            to: email,
+            subject:"Welcome to our MERN Auth System",
+            text:`Hi ${name},\n\nThank you for registering at our MERN Auth System. We're excited to have you on board!\n\nBest regards,\nMERN Auth Team`
+
+        }
+        await transporter.sendMail(mailOptions);
 
         return res.status(201).json({message:"User registered successfully",token:token})
 
